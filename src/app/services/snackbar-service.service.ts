fc59114550319467be9181eb3f00607e012a98ee
renatu <renatu_mb@yahoo.com.br>
2024-02-22 18:21:04 -0800

    Snackbar creation

---
+++ src/app/services/snackbar-service.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {MatSnackBar, MatSnackBarConfig, MatSnackBarModule} from '@angular/material/snack-bar';
+import {STATUS} from "../utils/status"
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SnackbarService {
+
+    private config: MatSnackBarConfig = {
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+        duration: 1500
+    }
+
+    constructor(private _snackBar: MatSnackBar) {
+    }
+
+    public open(message: string, status: STATUS): void {
+        this.config = {
+            ...this.config,
+            panelClass: status + '-snackbar'
+        }
+        this._snackBar.open(message, '', this.config)
+    }
+}
