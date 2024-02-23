545c465516bf891a55adcd02742f299b0c060caa
renatu <renatu_mb@yahoo.com.br>
2024-02-23 00:31:25 -0800

    Creation SpinnerComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MatIcon, MatIconModule} from "@angular/material/icon";
 import {CommonModule} from "@angular/common";
@@ -6,6 +6,8 @@ import {NavbarComponent} from "./pages/navbar/navbar.component";
 import {FooterComponent} from "./pages/footer/footer.component";
 import {HomeComponent} from "./pages/home/home.component";
 import {AuthComponent} from "./pages/auth/auth.component";
+import {SpinnerComponent} from "./utils/spinner/spinner.component";
+import {SpinnerService} from "./utils/spinner/spinner.service";
 
 @Component({
   selector: 'app-root',
@@ -17,11 +19,23 @@ import {AuthComponent} from "./pages/auth/auth.component";
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    AuthComponent
+    AuthComponent,
+    SpinnerComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend-recipe';
+export class AppComponent implements AfterViewInit {
+
+    @ViewChild(SpinnerComponent, {static: false})
+    targetSpinner !: ElementRef<SpinnerComponent>;
+
+    title = 'frontend-recipe';
+
+    constructor(private spinnerService: SpinnerService) {
+    }
+
+    ngAfterViewInit(): void {
+        this.spinnerService.setSpinner(this.targetSpinner)
+    }
 }
---
+++ src/app/utils/spinner/spinner.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+import {CommonModule} from "@angular/common";
+
+@Component({
+    selector: 'app-spinner',
+    standalone: true,
+    imports: [
+        CommonModule
+    ],
+    templateUrl: './spinner.component.html',
+    styleUrl: './spinner.component.css'
+})
+export class SpinnerComponent {
+    @Input('hide')
+    public hide: boolean = true;
+}
