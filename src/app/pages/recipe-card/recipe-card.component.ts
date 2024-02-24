1f7bbb1c327ea1d03143e3aa3e6e8dadbc8ff701
renatu <renatu_mb@yahoo.com.br>
2024-02-24 11:42:17 -0800

    Deletion of Recipe functionality

--- src/app/pages/recipe-card/recipe-card.component.ts
+++ src/app/pages/recipe-card/recipe-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from "@angular/material/icon";
@@ -8,6 +8,7 @@ import {EditRecipeFormComponent} from "../edit-recipe-form/edit-recipe-form.comp
 import {GENERIC_ERROR, Recipe, STATUS} from "../../utils/constants";
 import {RecipeService} from "../../services/recipe-service.service";
 import {SnackbarService} from "../../services/snackbar-service.service";
+import {SpinnerService} from "../../utils/spinner/spinner.service";
 
 @Component({
     selector: 'app-recipe-card',
@@ -30,10 +31,12 @@ export class RecipeCardComponent {
 
     @Input()
     enableEditDeleteButtons: boolean = false;
+    @Output() recipeDeleted = new EventEmitter<number>();
 
     constructor(public editRecipeDialog: MatDialog,
                 public recipeService: RecipeService,
-                public snackBarService: SnackbarService) {
+                public snackBarService: SnackbarService,
+                public spinnerService: SpinnerService) {
     }
 
     openEditRecipeComponent(recipe: Recipe): void {
@@ -53,6 +56,24 @@ export class RecipeCardComponent {
                     ...err
                 }
 
+                this.snackBarService.open(error.message, STATUS.ERROR)
+            }
+        })
+    }
+
+    removeRecipe(recipe: Recipe) {
+        this.spinnerService.open()
+        this.recipeService.deleteRecipe(recipe.id).subscribe({
+            next: value => {
+                this.spinnerService.close()
+                this.recipeDeleted.emit(recipe.id)
+                this.snackBarService.open(STATUS.SUCCESS.toUpperCase(), STATUS.SUCCESS)
+            }, error: err => {
+                this.spinnerService.close()
+                let error = {
+                    ...GENERIC_ERROR,
+                    ...err
+                }
                 this.snackBarService.open(error.message, STATUS.ERROR)
             }
         })
